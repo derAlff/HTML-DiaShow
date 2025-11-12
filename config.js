30239b5702c2eecc21681003399726be73ef78e1
derAlff <64785948+derAlff@users.noreply.github.com>
2025-11-12 12:14:50 +0100

    Add config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// === Slideshow Configuration ===
+const SLIDESHOW_CONFIG = {
+    SRC_DIR: './src/',          // Folder with pictures
+    SLIDE_MS: 5000,             // Duration per picture (in ms)
+    POLL_MS: 5000,              // Polling-Intervall (in ms)
+    EXTS: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.bmp']
+  };
